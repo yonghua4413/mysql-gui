94e4cc85c80a551d899bec6f11b7c141b6d13450
杨永华 <yangyonghua@yangyonghuadeMacBook-Pro.local>
2020-06-11 00:52:31 +0800

    init

---
+++ app/core/Register.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyonghua
+ * Date: 2020/6/11
+ * Time: 12:44 AM
+ */
+
+namespace App\core;
+
+
+class Register
+{
+
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangyonghua
+ * Date: 2020/6/11
+ * Time: 12:43 AM
+ */
